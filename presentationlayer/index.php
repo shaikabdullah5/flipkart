19df24e8fad9cc6df851d0d3d5f6f63278449c05
Shaikabdullah5 <shaikhabdullah5@outlook.com>
2016-03-02 15:20:12 +0530

    first commit

---
+++ presentationlayer/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Flipkart-Online Shopping Website</title>
+	<style type="text/css">
+	input {
+		background-color:#f2e394;
+		color:#588c7e;
+		width:250px;
+		padding: 15px;
+		border-radius:10px;
+
+	}
+	body{
+		background-color:#588c7e;
+	}
+	#tb1{
+		margin-top:150px ;
+	}
+
+	</style>
+</head>
+<body>
+    <form action="adminlogin.php">
+    	<table 	align="center" border="0" id="tb1">
+    	<tr>
+    		<td><input type="submit" value="ADMIN LOGIN"></td>
+    	</tr>
+    	</table>
+    </form>
+    <form action="customerlogin.php">
+    	<table 	align="center" border="0">
+    	<tr>
+    		<td><input type="submit" value="CUSTOMER LOGIN"></td>
+    	</tr>
+		</table>
+   </form>
+</body>
+</html>				
